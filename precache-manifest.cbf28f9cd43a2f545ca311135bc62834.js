61b5b4274c98b2c7635eebafb22f81f0c2668da0
tykosf <prromanyi@gmail.com>
2019-07-13 23:17:10 +0300

    add build

---
+++ precache-manifest.cbf28f9cd43a2f545ca311135bc62834.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b187d3d039cbe33674554bb81987492",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c3c6017d869f87e5660f",
+    "url": "/portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "0d56c9c1de79e32b8698",
+    "url": "/portfolio/static/css/main.53428f63.chunk.css"
+  },
+  {
+    "revision": "c3c6017d869f87e5660f",
+    "url": "/portfolio/static/js/2.296aa4d8.chunk.js"
+  },
+  {
+    "revision": "0d56c9c1de79e32b8698",
+    "url": "/portfolio/static/js/main.68377930.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "af7e4c6df2d4bf083ecd71928b75fa8c",
+    "url": "/portfolio/static/media/store.af7e4c6d.png"
+  },
+  {
+    "revision": "6f30e0663a075ab9b4ce714168083af1",
+    "url": "/portfolio/static/media/telbot.6f30e066.jpg"
+  },
+  {
+    "revision": "b09fc79076bb2e6bc30acbc45885e2e9",
+    "url": "/portfolio/static/media/weather.b09fc790.png"
+  },
+  {
+    "revision": "e03e9be2035d1cae9ca0c218f32361e1",
+    "url": "/portfolio/static/media/wms.e03e9be2.png"
+  }
+]);
